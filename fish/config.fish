197404c2081660aec6b2886de2b8ce7d142d2356
fm39hz <hitpoint2k3@gmail.com>
2023-09-03 22:34:56 +0700

    remove some alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias update-rice="~/.config/hypr/manage/update.sh"
-alias reload-rice="~/.config/hypr/waybar/start"
 set fish_greeting ""
