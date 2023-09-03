5dce68309e1ff1c99f6b01d3650c5ca473c933d2
fm39hz <hitpoint2k3@gmail.com>
2023-09-03 17:33:00 +0700

    first init

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias update-rice="~/.config/hypr/manage/update.sh"
+alias reload-rice="~/.config/hypr/waybar/start"
+set fish_greeting ""
