aa1337642cea9b79ae2846f3c570f4d86e3127e6
fm39hz <hitpoint2k3@gmail.com>
2023-09-05 15:48:01 +0700

    clean up environment

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,5 @@ export QT_IM_MODULE=ibus
 # Android Studio config
 export ANDROID_HOME=$HOME/Android/Sdk
 set -U fish_user_paths $ANDROID_HOME/emulator $fish_user_paths 
-set -U fish_user_paths $ANDROID_HOME/platforms-tools  $fish_user_paths 
+set -U fish_user_paths $ANDROID_HOME/platforms-tools $fish_user_paths 
 set fish_greeting ""
