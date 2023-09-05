b26a6c046d55b00b096d26a43d6179ddff262254
fm39hz <hitpoint2k3@gmail.com>
2023-09-05 15:13:06 +0700

    added android studio environment

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+# Ibus config
 export GTK_IM_MODULE=ibus
 export XMODIFIERS=@im=ibus
 export QT_IM_MODULE=ibus
+# Android Studio config
+export ANDROID_HOME=$HOME/Android/Sdk
+set -U fish_user_paths $ANDROID_HOME/emulator $fish_user_paths 
+set -U fish_user_paths $ANDROID_HOME/platforms-tools  $fish_user_paths 
 set fish_greeting ""
