f1a52e6ca90205c45e02b0bf57200ee108123b8d
FM39hz <71458213+fm39hz@users.noreply.github.com>
2024-01-03 00:39:24 +0700

    Update arch_setup.sh

--- arch_setup.sh
+++ arch_setup.sh
@@ -1,22 +1,25 @@
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
-yay -S cliphist hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland swww libnotify notification-daemon swaync networkmanager network-manager-applet bluez bluez-utils blueman papirus-icon-theme noto-fonts-emoji lxsession grimblast-git brightnessctl light nwg-look-bin yad sox mint-themes xcursor-simp1e-gruvbox-dark gtklock eww-wayland git xdg-desktop-portal-hyprland ttf-jetbrains-mono-nerd neofetch hyprshot zsh nitch lazygit
+#Install dependencies
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
+yay -S cliphist hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland swww libnotify notification-daemon swaync networkmanager network-manager-applet bluez bluez-utils blueman papirus-icon-theme noto-fonts-emoji lxsession grimblast-git brightnessctl light nwg-look-bin yad sox mint-themes xcursor-simp1e-gruvbox-dark gtklock eww-wayland thunar thunar-archive-plugin thorium-browser git xdg-desktop-portal-hyprland ttf-jetbrains-mono-nerd neofetch hyprshot zsh nitch lazygit
 
 #Setup config
 rm -rf ~/.config
 git clone git@github.com:fm39hz/.dotfiles.git ~/.config/
-mkdir ~/.config/backup
+mkdir -p ~/.config/backup/nvim/lua/custom
 
 #Setup NvChad
-cp ~/.config/nvim/lua/custom ~/.config/backup/nvim/lua/custom
+cp ~/.config/nvim/lua/custom ~/.config/backup/nvim/lua/custom -r
 rm -rf ~/.config/nvim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
 rm -rf ~/.config/nvim/lua/custom 
-cp ~/.config/backup/nvim/lua/custom ~/.config/nvim/lua/custom 
+cp ~/.config/backup/nvim/lua/custom ~/.config/nvim/lua/custom -r
 
-#Setup Shell
+#Setup Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#Symlink script & rc files
 ln -s ~/.config/.zshrc ~/.zshrc
-ln -s ~/.config/hyprland_autolog.sh /etc/profile.d/hyprland_autolog.sh
+sudo ln -s ~/.config/hyprland_autolog.sh /etc/profile.d/hyprland_autolog.sh
