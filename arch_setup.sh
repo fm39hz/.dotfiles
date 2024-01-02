2a423bf2b9415926f13634d5b258ad7b1a465f19
FM39hz <71458213+fm39hz@users.noreply.github.com>
2024-01-03 00:13:29 +0700

    Update arch_setup.sh

--- arch_setup.sh
+++ arch_setup.sh
@@ -1,10 +1,9 @@
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
-yay -S cliphist hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland swww libnotify notification-daemon swaync networkmanager network-manager-applet bluez bluez-utils blueman papirus-icon-theme noto-fonts-emoji lxsession grimblast-git brightnessctl light nwg-look-bin yad sox 
-mint-themes xcursor-simp1e-gruvbox-dark gtklock eww-wayland git xdg-desktop-portal-hyprland ttf-jetbrains-mono-nerd neofetch hyprshot zsh nitch lazygit
+yay -S cliphist hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland swww libnotify notification-daemon swaync networkmanager network-manager-applet bluez bluez-utils blueman papirus-icon-theme noto-fonts-emoji lxsession grimblast-git brightnessctl light nwg-look-bin yad sox mint-themes xcursor-simp1e-gruvbox-dark gtklock eww-wayland git xdg-desktop-portal-hyprland ttf-jetbrains-mono-nerd neofetch hyprshot zsh nitch lazygit
 
 #Setup config
 rm -rf ~/.config
-git clone https://github.com/fm39hz/.dotfiles.git ~/.config/
+git clone git@github.com:fm39hz/.dotfiles.git ~/.config/
 mkdir ~/.config/backup
 
 #Setup NvChad
@@ -16,7 +15,8 @@ cp ~/.config/backup/nvim/lua/custom ~/.config/nvim/lua/custom
 
 #Setup Shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+rm ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-wget https://raw.githubusercontent.com/unxsh/nitch/main/setup.sh && sh setup.sh
 ln -s ~/.config/.zshrc ~/.zshrc
+ln -s ~/.config/hyprland_autolog.sh /etc/profile.d/hyprland_autolog.sh
