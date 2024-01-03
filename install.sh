19f5f04bb141d671a7d6da8e10d1876b225b46a7
FM39hz <71458213+fm39hz@users.noreply.github.com>
2024-01-03 13:56:16 +0700

    added dependencies

--- install.sh
+++ install.sh
@@ -1,26 +1,4 @@
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
-yay -S cliphist hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland swww libnotify notification-daemon swaync networkmanager network-manager-applet bluez bluez-utils blueman papirus-icon-theme noto-fonts-emoji lxsession grimblast-git brightnessctl light nwg-look-bin yad sox mint-themes xcursor-simp1e-gruvbox-dark gtklock eww-wayland git xdg-desktop-portal-hyprland ttf-jetbrains-mono-nerd neofetch hyprshot
-echo "Do you wish to install NvChad? (y/N):"
-select yn in "y" "N"; do
-    case $yn in
-        y ) git clone https://github.com/NvChad/NvChad.git ~/.config/nvim;;
-        N ) exit;;
-
-
-echo "Do you wish to install Oh-my-zsh? (y/N):"
-select yn in "y" "N"; do
-    case $yn in
-        y ) sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; break;;
-        N ) exit;;
-    esac
-done
-git clone https://github.com/fm39hz/.dotfiles.git ~/.config
-
-echo "Do you want to autologin to Hyprland with non root account? (y/N):"
-
-select yn in "y" "N"; do
-    case $yn in
-        y ) cp ~/.config/hyprland_autolog.sh /etc/profile.d/; break;;
-        N ) exit;;
-    esac
-done
+#Install dependencies
+sudo pacman -S git npm unzip zsh neofetch 
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
+yay -S cliphist hyprland-git waybar-hyprland-git alacritty rofi-lbonn-wayland swww libnotify notification-daemon swaync networkmanager network-manager-applet bluez bluez-utils blueman papirus-icon-theme noto-fonts-emoji lxsession grimblast-git brightnessctl light nwg-look-bin yad sox mint-themes xcursor-simp1e-gruvbox-dark gtklock eww-wayland thunar thunar-archive-plugin thorium-browser xdg-desktop-portal-hyprland ttf-jetbrains-mono-nerd hyprshot nitch lazygit
