6f8d4944ab63ac20bb52b87da42b0d540b6c48df
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-04-24 10:12:52 +0700

    feat: add nvchad default nvim config

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ rm -rf ~/.config
 git clone git@github.com:fm39hz/.dotfiles.git ~/.config/
 
 #Setup NvChad
-git clone git@github.com:fm39hz/NvChad.git ~/.config/nvim/ --depth 1 && nvim
+git clone git@github.com:fm39hz/NvChad.git ~/.config/nvim-nvchad/ --depth 1 && nvim
 
 #Setup Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -14,8 +14,13 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 
 #Start service
 sudo systemctl enable greetd.service
+sudo systemctl enable bluetooth.service
 
 #Symlink script & rc files
 ln -s ~/.config/.zshrc ~/.zshrc
+ln -s ~/.config/nvim-nvchad ~/.config/nvim
+ln -s ~/.local/share/nvim-nvchad ~/.local/share/nvim
+ln -s ~/.local/state/nvim-nvchad ~/.local/state/nvim
+ln -s ~/.cache/nvim-nvchad ~/.cache/nvim
 sudo ln -S ~/.config/greetd/config.toml /etc/greetd/config.toml
 sudo ln -s ~/.config/hyprland_autolog.sh /etc/profile.d/hyprland_autolog.sh
