eace5b7d4bfef5f68b6b692e1e2784a414b4f0c2
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-05-13 15:43:27 +0700

    feat: change wallpaper

--- hypr/lib.sh
+++ hypr/lib.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-
-
-set_wallpaper () {
-  wbg ~/.config/Wallpaper/AestheticCity.jpg &
+set_wallpaper() {
+	wbg ~/.config/Wallpaper/japanese_pedestrian_street.png &
 }
 
-run_hook () {
+run_hook() {
 	$HOME/.hyprland_rice/autostart_$1
 }
 
-eww-rice () {
+eww-rice() {
 	eww --config ~/.config/eww/ $*
 }
