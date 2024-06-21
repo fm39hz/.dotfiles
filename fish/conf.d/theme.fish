5981e4d072053914d787102a3838af81ea77658c
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-21 22:30:20 +0700

    feat: initialize fish & omf

---
+++ fish/conf.d/theme.fish
@@ -0,0 +1,9 @@
+set -g theme_nerd_fonts yes
+set -g theme_powerline_fonts no
+set -g theme_display_git yes
+set -g theme_display_user yes
+set -g theme_display_vi yes
+set -g theme_display_hostname yes
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '~~> '
+set -g theme_color_scheme nord
---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,4 @@
+function fish_user_key_bindings
+    bind \e\e thefuck-command-line
+    fish_vi_key_bindings
+end
