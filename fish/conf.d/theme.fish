291cfdb7e05209b134d3448e22b1d88eaeaaceb4
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-24 16:16:36 +0700

    feat: change new line prompt

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -5,5 +5,6 @@ set -g theme_display_user yes
 set -g theme_display_vi yes
 set -g theme_display_hostname yes
 set -g theme_newline_cursor yes
-set -g theme_newline_prompt '~~> '
+# set -g theme_newline_prompt '~~> '
+set -g theme_newline_prompt '  '
 set -g theme_color_scheme nord
