479a0ecf6ab55d740275df3dd2efd73c957d467f
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-25 10:38:06 +0700

    chore: minor changes

--- fish/conf.d/theme.fish
+++ fish/conf.d/theme.fish
@@ -6,5 +6,5 @@ set -g theme_display_vi yes
 set -g theme_display_hostname yes
 set -g theme_newline_cursor yes
 # set -g theme_newline_prompt '~~> '
-set -g theme_newline_prompt '  '
+set -g theme_newline_prompt '[  '
 set -g theme_color_scheme nord
