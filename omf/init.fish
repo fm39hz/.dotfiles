673235c0cf5ea824cc52b22bd93713200f1a8c1f
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-06-25 20:17:48 +0700

    fix: fix path add

--- omf/init.fish
+++ omf/init.fish
@@ -17,10 +17,13 @@ alias nvim_delete "~/.config/scripts/nvim_delete.sh"
 
 # Android Dev environment
 set -U ANDROID_HOME "$HOME/Android/Sdk"
-set -U PATH $PATH:$ANDROID_HOME/platform-tools/
-set -U PATH $PATH:$ANDROID_HOME/tools/bin/
-set -U PATH $PATH:$ANDROID_HOME/emulator
-set -U PATH $PATH:$ANDROID_HOME/tools/
+fish_add_path -U "$ANDROID_HOME/platform-tools/"
+fish_add_path -U "$ANDROID_HOME/tools/bin/"
+fish_add_path -U "$ANDROID_HOME/emulator"
+fish_add_path -U "$ANDROID_HOME/tools/"
+
+# Dotnet path
+fish_add_path -U "$HOME/.dotnet/tools"
 
 # Flutter environment
 # set -U PATH "$PATH:$HOME/.pub-cache/bin"
@@ -29,7 +32,7 @@ set -U CHROME_EXECUTABLE thorium-browser
 # GodotEnv environment
 alias godotenv "$HOME/.dotnet/tools/godotenv"
 set -U GODOT "$HOME/.config/godotenv/godot/bin/godot"
-set -U PATH "$HOME/.config/godotenv/godot/bin:$PATH"
+fish_add_path -U "$HOME/.config/godotenv/godot/bin:$PATH"
 
 # Docker alias
 alias dockerup "docker-compose --log-level ERROR up -d --build"
