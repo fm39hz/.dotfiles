434502b4b95c8e9c48171f48305c47f02b4388c3
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-07-16 13:35:26 +0700

    chore: better escape for vim mode

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,5 @@
 function fish_user_key_bindings
     bind \e\e thefuck-command-line
+    bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
     fish_vi_key_bindings
 end
