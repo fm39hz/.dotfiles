b76390769e58a18d6b7a8cadb8b51aec9c5003bb
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-07-17 15:06:21 +0700

    feat: implement Bobthe Fish everforest

---
+++ fish/functions/bobthefish_colors.fish
@@ -0,0 +1,29 @@
+# function bobthefish_colors -S -d 'Define a custom bobthefish color scheme'
+#     # then override everything you want! note that these must be defined with `set -x`
+#     #           <--name-->               <--foreground--> <--background--> <--options-->
+#     set -x color_initial_segment_exit #     bg_red          543A48
+#     set -x color_initial_segment_private #  bg_yellow       DBBC7F
+#     set -x color_initial_segment_su #       bg_green        A7C080          --bold
+#     set -x color_initial_segment_jobs #     bg_yellow       DBBC7F          --bold
+#     set -x color_path #                     bg1             2D353B
+#     set -x color_path_basename #            fg              D3C6AA          --bold
+#     set -x color_path_nowrite #             bg_red          543A48
+#     set -x color_path_nowrite_basename #    fg              D3C6AA          --bold
+#     set -x color_repo #                     bg_green        A7C080
+#     set -x color_repo_work_tree #           fg              D3C6AA          --bold
+#     set -x color_repo_dirty #               bg_red          543A48
+#     set -x color_repo_staged #              yellow          DBBC7F
+#     set -x color_vi_mode_default #          bg1             2D353B
+#     set -x color_vi_mode_insert #           bg_green        A7C080          --bold
+#     set -x color_vi_mode_visual #           yellow          DBBC7F
+#     set -x color_vagrant #                  bg_blue         7FBBB3          --bold
+#     set -x color_aws_vault
+#     set -x color_aws_vault_expired
+#     set -x color_username #                 fg              D3C6AA          --bold
+#     set -x color_hostname #                 fg              D3C6AA          --bold
+#     set -x color_rvm #                      bg_red          543A48
+#     set -x color_virtualfish #              bg_blue         7FBBB3          --bold
+#     set -x color_virtualgo #                bg_blue         7FBBB3          --bold
+#     set -x color_desk #                     bg_blue         7FBBB3          --bold
+#     set -x color_nix #                      bg_blue         7FBBB3          --bold
+# end
