b4f84a3de02b80e6b70dc809f2939aab2c68d707
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-10-09 12:01:12 +0700

    refactor: clean up fish setup

--- omf/init.fish
+++ omf/init.fish
@@ -4,6 +4,10 @@ set -U fish_escape_delay_ms 10
 set -U fish_cursor_default block
 set -U fish_cursor_insert line
 
+# Color config
+set -U fish_color_selection --background=7A8478 --foreground=232A2E
+set -U fish_color_search_match --background=7A8478 --foreground=232A2E
+
 # Convenient config
 set -U LANG en_US.UTF-8
 set -U LC_ALL en_US.UTF-8
