18e2599b9f05a221bdb8b67e45c1e2045a49bc04
fm39hz-laptop <hitpoint2k3@gmail.com>
2024-10-16 13:13:42 +0700

    feat: add system update Key

---
+++ scripts/system_update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+
+# Update system packages using yay
+echo "Updating system packages with yay..."
+yay --noconfirm
+
+# Update Oh My Fish and any installed plugins
+echo "Updating Oh My Fish and plugins..."
+omf update
+
+echo "Updating Nvim packages..."
+nvim --headless "+Lazy! sync" +qa
+cd ~/.config/nvim/ || exit
+
+# Check if there are any changes to commit
+if git diff --quiet lazy-lock.json
+    echo "No changes to commit for Nvim packages."
+else
+    git add lazy-lock.json
+    git commit -m "chore: update deps"
+    git push
+end
+
+echo "Updating Steam"
+steam -silent
+
+echo "Update Complete!"
