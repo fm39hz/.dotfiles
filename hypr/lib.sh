2bc6376a10aaa24614753fe9c8888bfe71ef3303
FM39hz <hitpoint2k3@gmail.com>
2024-11-14 01:27:46 +0700

    chore: clean up some old config

--- hypr/lib.sh
+++ hypr/lib.sh
@@ -4,10 +4,6 @@ set_wallpaper() {
 	wbg ~/.config/Wallpaper/japanese_pedestrian_street.png &
 }
 
-run_hook() {
-	$HOME/.hyprland_rice/autostart_$1
-}
-
 eww-rice() {
 	eww --config ~/.config/eww/ $*
 }
