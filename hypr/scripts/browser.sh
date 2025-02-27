0558954130f9bf9ba692c8f86a2d60d7b9f6f774
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-02-27 09:50:49 +0700

    fix(browser): enable wayland ime support

--- hypr/scripts/browser.sh
+++ hypr/scripts/browser.sh
@@ -2,4 +2,4 @@
 
 source "$HOME"/.config/hypr/lib.sh
 
-manage_focus "brave-browser-nightly" "brave-browser-nightly" "3" "true" "--enable-features=TouchpadOverscrollHistoryNavigation --password-store=basic"
+manage_focus "Brave-browser-nightly" "brave-browser-nightly" "3" "true" "--enable-features=TouchpadOverscrollHistoryNavigation,vulkan,vulkanfromangle,defaultanglevulkan,vaapiignoredriverchecks,vaapivideodecoder,usemultiplaneformatforhardwarevideo,vaapivideoencoder --password-store=basic --enable-wayland-ime"
