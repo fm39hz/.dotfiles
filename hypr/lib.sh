a33c654db4089fc19cc09808580976e4973818be
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-02-27 08:39:27 +0700

    feat: add Brave config
    feat: switch to brave

--- hypr/lib.sh
+++ hypr/lib.sh
@@ -8,31 +8,49 @@ eww-rice() {
 	eww --config ~/.config/eww/ $*
 }
 
-# Function to manage application focus in Hyprland based on app name and class
-# Usage: manage_focus <app_name> <app_class>
+# Function to manage application focus in Hyprland based on app name/class
+# Usage: manage_focus <search_term> <app_class> [<target_workspace>] [<search_by_class>] [<extra_args>]
 manage_focus() {
-	local app_name="$1"
+	local search_term="$1"
 	local app_class="$2"
 	local target_workspace="$3"
+	local search_by_class="${4:-false}" # Optional parameter, defaults to false
+	local extra_args="${5:-""}"         # Optional parameter, defaults to empty string
+	local grep_pattern=""
+
+	# Set the grep pattern based on whether we're searching by class or title
+	if [[ "$search_by_class" == "true" ]]; then
+		grep_pattern="class: $search_term"
+	else
+		grep_pattern="$search_term"
+	fi
 
 	# Function to retrieve the workspace information by column number
 	get_workspace() {
-		hyprctl clients | grep -A 10 "$app_name" | grep "workspace" | awk -v col="$1" '{print $col}' | head -n 1
+		local pattern="$1"
+		local col="$2"
+		hyprctl clients | rg -B 5 "$pattern" | grep "workspace" | awk -v col="$col" '{print $col}' | head -n 1
 	}
 
-	# Check if any window with the app name exists
-	if hyprctl clients | grep -q "$app_name"; then
+	# Check if any window with the search term exists
+	if hyprctl clients | rg -q "$grep_pattern"; then
 		# If an app window is open, find its workspace
-		target_workspace=$(get_workspace 2)
+		target_workspace=$(get_workspace "$grep_pattern" 2)
+		echo "Found $search_term on workspace $target_workspace"
 		# Check if the workspace is numeric and greater than 0
-		if [[ ! "$target_workspace" =~ ^[0-9]+$ ]] && ((target_workspace < 0)); then
-			target_workspace=$(get_workspace 3 | sed 's/[()]//g') # Remove parentheses from workspace name
+		if [[ ! "$target_workspace" =~ ^[0-9]+$ ]] || ((target_workspace < 0)); then
+			target_workspace=$(get_workspace "$grep_pattern" 3 | sed 's/[()]//g') # Remove parentheses from workspace name
 		fi
 		# Focus on the workspace
 		hyprctl dispatch workspace "$target_workspace"
 	else
 		# If the app is not running, start it
 		hyprctl dispatch workspace "$target_workspace"
-		$app_class &
+		# Use -- to indicate the end of options for browsers like Brave
+		if [[ "$app_class" == "brave" || "$app_class" == "brave-browser" ]]; then
+			$app_class -- $extra_args &
+		else
+			$app_class $extra_args &
+		fi
 	fi
 }
