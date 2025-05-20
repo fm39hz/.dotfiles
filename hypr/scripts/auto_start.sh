de96a861ab7ea8861d9421a28ea2643e2c447e4f
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-05-20 13:29:00 +0700

    chore(hypr): update autostart and hyprlock config

--- hypr/scripts/auto_start.sh
+++ hypr/scripts/auto_start.sh
@@ -2,25 +2,19 @@
 
 source "$HOME"/.config/hypr/lib.sh
 
-swww init
-
-set_wallpaper
-
 eval "hypridle" &
 
-~/.config/swaync/start
-
-eval "hyprlock" &
-
-nm-applet &
-blueman-applet &
-
-lxsession &
-
-brightnessctl --restore
-
-eval "hyprctl setcursor everforest-cursors 32;" &
-
-eval "sleep 1; fcitx5" &
-# eval "localsend; sleep 15; hyprctl dipatch closewindow localsend" &
-eval "sleep 0.5; hyprctl reload" &
+# swww init
+#
+# set_wallpaper
+#
+# ~/.config/swaync/start
+#
+# eval "hyprlock" &
+#
+# nm-applet &
+# blueman-applet &
+#
+# lxsession &
+#
+# eval "sleep 0.5; hyprctl reload" &
