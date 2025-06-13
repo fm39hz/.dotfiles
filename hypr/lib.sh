6cd104ab1c81818e1ca4a0b5e40f089fb3816533
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-06-14 01:01:47 +0700

    refactor: Extract window search logic into `find_apps` function

--- hypr/lib.sh
+++ hypr/lib.sh
@@ -3,6 +3,19 @@
 # set_wallpaper() {
 # 	wbg ~/.config/Wallpaper/japanese_pedestrian_street.png &
 # }
+function find_apps() {
+	local search_term="$1"
+	local search_by_class="${3:-false}"
+	local window_pattern
+	if [[ "$search_by_class" == "true" ]]; then
+		# When searching by class, use the appropriate regex pattern
+		window_pattern="class: ($search_term)"
+	else
+		# When searching by title, use the search term directly
+		window_pattern="$search_term"
+	fi
+	echo window_pattern
+}
 
 # Function to bring a window to the current workspace in Hyprland
 # Usage: bring_window_to_current <search_term> <app_class> [<search_by_class>] [<extra_args>]
@@ -13,15 +26,7 @@ bring_window_to_current() {
 	local extra_args="${4:-}"
 
 	# Construct the window search pattern
-	local window_pattern
-	if [[ "$search_by_class" == "true" ]]; then
-		# When searching by class, use the appropriate regex pattern
-		window_pattern="class: ($search_term)"
-	else
-		# When searching by title, use the search term directly
-		window_pattern="$search_term"
-	fi
-
+	window_pattern=$(find_apps search_term search_by_class)
 	# Construct the command to execute if window isn't found
 	local cmd
 	if [[ -n "$extra_args" ]]; then
@@ -81,5 +86,43 @@ manage_focus() {
 		return
 	fi
 	hyprctl dispatch workspace "$target_workspace"
-	hyprctl dispatch plugin:xtd:moveorexec "$grep_pattern", "uwsm app -- $app_class"
+	hyprctl dispatch plugin:xtd:moveorexec "$grep_pattern", "app2unit $app_class"
+}
+function close_apps() {
+	BRAVE=$(find_apps brave-browser-nightly | wc -l)
+	CHROMIUM=$(find_apps chromium true | wc -l)
+	FIREFOX=$(find_apps firefox true | wc -l)
+
+	if [ "$BRAVE" -gt "1" ]; then
+		notify-send "power controls" "Brave multiple windows open"
+		exit 1
+	elif [ "$CHROMIUM" -gt "1" ]; then
+		notify-send "power controls" "Chromium multiple windows open"
+		exit 1
+	elif [ "$FIREFOX" -gt "1" ]; then
+		notify-send "power controls" "Firefox multiple windows open"
+		exit 1
+	fi
+
+	sleep 3
+
+	# close all client windows
+	# required for graceful exit since many apps aren't good SIGNAL citizens
+	HYPRCMDS=$(hyprctl -j clients | jq -j '.[] | "dispatch closewindow address:\(.address); "')
+	temp_dir="tmp/hypr"
+	mkdir -p $temp_dir
+	hyprctl --batch "$HYPRCMDS" >>$temp_dir/hyprexitwithgrace.log 2>&1
+
+	notify-send "Power controls" "Closing Applications..."
+
+	sleep 2
+
+	COUNT=$(find_apps "class:" | wc -l)
+	if [ "$COUNT" -eq "0" ]; then
+		notify-send "Power controls" "Closed Applications."
+		return
+	else
+		notify-send "Power controls" "Some apps didn't close. Not shutting down."
+		exit 1
+	fi
 }
