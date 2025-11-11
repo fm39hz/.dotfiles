809712dbf8dd19c54e075d12654a7c0e076d84b4
fm39hz <hitpoint2k3@gmail.com>
2025-11-11 16:45:02 +0700

    fix(shell): fix fish shell keybinding conflict

--- fish/functions/fish_user_key_bindings.fish
+++
@@ -1,5 +0,0 @@
-function fish_user_key_bindings
-    bind \e\e thefuck-command-line
-    bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
-    fish_vi_key_bindings --no-erase insert
-end
--- omf/key_bindings.fish
+++ omf/key_bindings.fish
@@ -1,2 +1,5 @@
-bind ctrl-f thefuck-command-line
-bind ctrl-b 'if test -z "$TMUX" ~/.config/scripts/tmux_project.sh $argv end' repaint
+bind ctrl-b '
+    if test -z "$TMUX"
+    ~/.config/scripts/tmux_project.sh $argv 
+    end
+    ' repaint
